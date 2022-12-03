0290017c708955e4b62f540f5fe9e360dcb94bba
Metwally SAyed <Metwallysayed1999@gmail.com>
2022-12-03 14:31:12 +0200

    hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Aboutus from './components/Aboutus/Aboutus';
 import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div>
+      <Hero />
       <Aboutus />
       <Footer />
     </div>
